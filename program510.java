23324127ab7bfd1337f195711212f702bfcf1bc4
Bhakti Sarkale <141128858+bhaktis27@users.noreply.github.com>
2024-08-25 23:46:34 +0530

    Add files via upload

---
+++ program510.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+class program510
+{
+    public static void main(String Arg[]) throws Exception
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("------Marvellous Packer Unpacker CUI Module--------");
+        
+        System.out.println("Enter File name that you want to open for writing: ");
+        String File_Name = sobj.nextLine();
+
+        File fobj = new File(File_Name);
+
+        if(fobj.exists())
+        {
+            FileInputStream fiobj = new FileInputStream(fobj);
+
+            byte Arr[] = new byte[20];
+
+            fiobj.read(Arr);
+            
+            System.out.println("Data from file is : "+Arr);
+        }
+        else 
+        {
+            System.out.println("There is no such file");
+        }
+    }
+}
